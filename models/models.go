80a30cc5b0c70813a05f0a2e11412d03880c2a79
ahtalbi <obetox1@gmail.com>
2025-08-07 08:45:27 +0100

    modify struct id to int mistake

--- models/models.go
+++ models/models.go
@@ -20,7 +20,7 @@ var (
 
 // artists
 type Artist struct {
-	Id           string   `json:"id"`
+	Id           int   `json:"id"`
 	Image        string   `json:"image"`
 	Name         string   `json:"name"`
 	Members      []string `json:"members"`
